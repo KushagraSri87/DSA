62b16a59de97ef9fa05741b8b8ba63087b02afe0
Kushagra Srivastava <srivastavakushagra2005@gmail.com>
2026-07-28 19:43:05 +0530

    Rev LL

--- Linked List/ReverseLL.cpp
+++ Linked List/ReverseLL.cpp
@@ -1,29 +1,49 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
+#include<iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* reverseList(ListNode* head){
     ListNode* prev = nullptr;
     ListNode* curr = head;
     ListNode* next = nullptr;
 
-    while (curr != nullptr) {
-        next = curr->next;  
-        curr->next = prev;   
-        prev = curr;         
-        curr = next;         
+    while(curr != nullptr){
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
     }
 
-    return prev;             
+    return prev;
 }
-        
-    
-};
+
+void printList(ListNode* head){
+    while(head != nullptr){
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main(){
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original: ";
+    printList(head);
+
+    head = reverseList(head);
+
+    cout << "Reversed: ";
+    printList(head);
+
+    return 0;
+}
