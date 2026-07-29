70dbbf24077cbf021011de430170271d397d0b76
Kushagra Srivastava <srivastavakushagra2005@gmail.com>
2026-07-29 19:40:36 +0530

    Added optimal solution

--- Leetcode/Pivotindex.cpp
+++ Leetcode/Pivotindex.cpp
@@ -25,3 +25,42 @@ return -1;
         
     }
 };
+// Optimesed solution 
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+
+        int n = nums.size();
+
+        // Create prefix array
+        vector<int> prefix(n);
+        prefix[0] = nums[0];
+
+        // Build prefix sum array
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+
+        // Check every index as a potential pivot
+        for (int i = 0; i < n; i++) {
+
+            int leftSum;
+            int rightSum;
+
+            // Special case for first element
+            if (i == 0)
+                leftSum = 0;
+            else
+                leftSum = prefix[i - 1];
+
+            // Sum of elements after i
+            rightSum = prefix[n - 1] - prefix[i];
+
+            if (leftSum == rightSum)
+                return i;
+        }
+
+        return -1;
+    }
+};
