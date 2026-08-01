a8cf9c227343686d6310e576f84bc7a86950b623
Kushagra Srivastava <srivastavakushagra2005@gmail.com>
2026-08-01 18:23:20 +0530

    Improved the complexity

--- Leetcode/11_Container_With_Most_Water.cpp
+++ Leetcode/11_Container_With_Most_Water.cpp
@@ -19,3 +19,35 @@ public:
 }
 
 };
+
+// optimized solution
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size()-1;
+        int maxArea=0;
+        while(left<right){
+            int width = right - left;
+            int waterHeight = min(height[right], height[left]);
+            int area = waterHeight * width;
+            if(area>maxArea){
+                maxArea=area;
+                }
+            if (height[left] < height[right]) {
+                left++;
+                }
+            else {
+                right--;
+    }
+            
+    
+    
+
+        }
+        return maxArea;
+
+}
+
+};
