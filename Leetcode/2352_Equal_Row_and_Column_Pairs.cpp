bdd81ef23e874311aaa83a2210258f129000c22d
Kushagra Srivastava <srivastavakushagra2005@gmail.com>
2026-08-06 19:05:56 +0530

    New question Added

--- Leetcode/2352_Equal_Row_and_Column_Pairs.cpp
+++ Leetcode/2352_Equal_Row_and_Column_Pairs.cpp
@@ -25,3 +25,34 @@ public:
         return count;
     }
 };
+//optimized solution
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+
+        int n = grid.size();
+        int count = 0;
+
+        map<vector<int>, int> freq;
+
+        // Store all rows and their frequencies
+        for (int i = 0; i < n; i++) {
+            freq[grid[i]]++;
+        }
+
+        // Build each column
+        for (int j = 0; j < n; j++) {
+
+            vector<int> column;
+
+            for (int i = 0; i < n; i++) {
+                column.push_back(grid[i][j]);
+            }
+
+            // Number of rows equal to this column
+            count += freq[column];
+        }
+
+        return count;
+    }
+};
