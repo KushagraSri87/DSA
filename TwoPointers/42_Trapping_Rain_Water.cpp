663aa271ef69fffb7b79847421eb457dad139348
Kushagra Srivastava <srivastavakushagra2005@gmail.com>
2026-08-15 21:11:19 +0530

    Leetcode 42

---
+++ TwoPointers/42_Trapping_Rain_Water.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int water = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int leftMax = height[i];
+            for (int j = 0; j <= i; j++) {
+                leftMax = max(leftMax, height[j]);
+            }
+
+            int rightMax = height[i];
+            for (int j = i; j < n; j++) {
+                rightMax = max(rightMax, height[j]);
+            }
+
+            water += min(leftMax, rightMax) - height[i];
+        }
+
+        return water;
+    }
+};
