eb0b8f7b4aeddeeb9768386945ada52e3ad9d2ba
Kushagra Srivastava <srivastavakushagra2005@gmail.com>
2026-08-31 19:46:30 +0530

    Binary search

---
+++ BInarySearch/704_Binary_Search.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int key){
+        int start = 0;
+        int end = arr.size()-1;
+        int mid = start + (end-start)/2;
+
+        while(start<=end){
+            if(arr[mid]==key){
+                return mid;
+            }
+            if(key>arr[mid]){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+            mid = start + (end-start)/2;
+        }
+        return -1;
+    }
+};
